d579df389a40d502d0cd79a3bfea496b713394d5
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-11-07 14:37:27 +0900

    [feat] 네이버 맵 마커 구현 및 클릭 리스너 구현

--- app/src/main/java/com/example/corona19vaccinemapservice/UI/MapFragment.kt
+++ app/src/main/java/com/example/corona19vaccinemapservice/UI/MapFragment.kt
@@ -13,19 +13,31 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.corona19vaccinemapservice.R
 import com.example.corona19vaccinemapservice.databinding.MapFragmentBinding
+import com.example.corona19vaccinemapservice.model.Data
+import com.example.corona19vaccinemapservice.model.VaccineStations
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
+import com.naver.maps.map.overlay.OverlayImage
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback, Overlay.OnClickListener {
 
     companion object {
         fun newInstance() = MapFragment()
     }
 
     private lateinit var viewModel: MapViewModel
-
     private lateinit var binding : MapFragmentBinding
 
     private lateinit var navController: NavController
 
+    private lateinit var naverMap: NaverMap
+
+    private lateinit var list : ArrayList<Data>
+
+    var count = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,10 +55,59 @@ class MapFragment : Fragment() {
         val args : MapFragmentArgs by navArgs()
 
         val item = args.item
-        var map= item.stationMap
+        setMarker(item)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync { naverMap = it }
 
-        Toast.makeText(context, map.get(1)?.get(0)!!.centerName,Toast.LENGTH_SHORT).show()
+
+
 
     }
 
+
+    fun setMarker(item : VaccineStations){
+        var stationMap = item.stationMap
+        list = ArrayList()
+        var index=0
+        for(key in stationMap.keys){
+            var data = stationMap[key]
+
+
+
+            for(i in data!!.indices){
+                var marker = Marker()
+                list.add(data[i])
+                marker.position = LatLng(data[i].lat!!.toDouble(),data[i].lng!!.toDouble())
+                marker.zIndex = index++
+
+                if(data[i].centerType=="중앙/권역"){
+                    marker.icon = OverlayImage.fromResource(R.drawable.ic_location_on_yellow_24)
+                }else if(data[i].centerType=="지역"){
+                    marker.icon = OverlayImage.fromResource(R.drawable.ic_location_on_green_24)
+                }
+                marker.setOnClickListener(this)
+                marker.width = 100
+                marker.height = 100
+                binding.mapView.getMapAsync {
+                    marker.map = it
+                }
+
+            }
+
+
+        }
+    }
+
+
+
+    override fun onMapReady(p0: NaverMap) {
+    }
+
+    override fun onClick(overlay: Overlay): Boolean {
+      if(overlay is Marker){
+          Toast.makeText(context, list[overlay.zIndex].address, Toast.LENGTH_SHORT).show()
+          return true
+      }
+        return true
+    }
 }
