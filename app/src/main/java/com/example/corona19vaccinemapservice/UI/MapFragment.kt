344cdd3e60085e75d52098f5d64c3be5ad359525
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-11-07 20:25:03 +0900

    [feat] 마커 클릭 시 상세화면 구현

--- app/src/main/java/com/example/corona19vaccinemapservice/UI/MapFragment.kt
+++ app/src/main/java/com/example/corona19vaccinemapservice/UI/MapFragment.kt
@@ -16,6 +16,8 @@ import com.example.corona19vaccinemapservice.databinding.MapFragmentBinding
 import com.example.corona19vaccinemapservice.model.Data
 import com.example.corona19vaccinemapservice.model.VaccineStations
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
@@ -36,6 +38,8 @@ class MapFragment : Fragment(), OnMapReadyCallback, Overlay.OnClickListener {
     private lateinit var naverMap: NaverMap
 
     private lateinit var list : ArrayList<Data>
+
+    private  var currentClickIndex=-1
 
     var count = 0
     override fun onCreateView(
@@ -61,6 +65,11 @@ class MapFragment : Fragment(), OnMapReadyCallback, Overlay.OnClickListener {
 
 
 
+        binding.VaccineStationExit.setOnClickListener {
+            currentClickIndex = -1
+
+            binding.vaccineStationLayout.visibility = View.GONE
+        }
 
     }
 
@@ -86,8 +95,8 @@ class MapFragment : Fragment(), OnMapReadyCallback, Overlay.OnClickListener {
                     marker.icon = OverlayImage.fromResource(R.drawable.ic_location_on_green_24)
                 }
                 marker.setOnClickListener(this)
-                marker.width = 100
-                marker.height = 100
+                marker.width = 140
+                marker.height = 140
                 binding.mapView.getMapAsync {
                     marker.map = it
                 }
@@ -105,7 +114,34 @@ class MapFragment : Fragment(), OnMapReadyCallback, Overlay.OnClickListener {
 
     override fun onClick(overlay: Overlay): Boolean {
       if(overlay is Marker){
-          Toast.makeText(context, list[overlay.zIndex].address, Toast.LENGTH_SHORT).show()
+
+          var cindex = overlay.zIndex
+          if(currentClickIndex!=cindex){
+              var data = list[cindex]
+              val camera = CameraUpdate.scrollTo(LatLng(data.lat!!.toDouble(),data.lng!!.toDouble())).animate(CameraAnimation.Easing)
+              overlay.map!!.moveCamera(camera)
+
+              binding.Phonenumber.setText(data.phoneNumber)
+              binding.vaccineStationAddress.setText(data.address)
+              binding.centerName.setText(data.centerName)
+              binding.vaccineStationName.setText(data.facilityName)
+              binding.updateAt.setText(data.updatedAt)
+              binding.vaccineStationLayout.visibility = View.VISIBLE
+              currentClickIndex = cindex
+
+              if(data.centerType=="중앙/권역"){
+                  binding.vaccineStationImg.setImageResource(R.drawable.ic_outline_location_city_24)
+              }else{
+                  binding.vaccineStationImg.setImageResource(R.drawable.ic_outline_location_city_green_24)
+
+              }
+          }else{
+              currentClickIndex = -1
+
+              binding.vaccineStationLayout.visibility = View.GONE
+          }
+
+
           return true
       }
         return true
---
+++ app/src/main/java/com/example/corona19vaccinemapservice/UI/VaccineStationDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.corona19vaccinemapservice.ui
+
+import androidx.lifecycle.ViewModel
+
+class VaccineStationDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
